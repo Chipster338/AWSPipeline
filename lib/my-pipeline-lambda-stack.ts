55d74ce1f6a0904781a712e7e9995c73bfaa6dd2
Chiapponi <luchiap@amazon.com>
2022-06-24 14:50:02 -0500

    initial commit

--- lib/my-pipeline-lambda-stack.ts
+++ lib/my-pipeline-lambda-stack.ts
@@ -1,15 +1,17 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-import { Function, InlineCode, Runtime } from 'aws-cdk-lib/aws-lambda';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+
 
 export class MyLambdaStack extends cdk.Stack {
     constructor(scope: Construct, id: string, props?: cdk.StackProps) {
       super(scope, id, props);
   
-      new Function(this, 'LambdaFunction', {
-        runtime: Runtime.NODEJS_12_X,
+      
+      const myFunctionHandler = new lambda.Function(this, 'MyFunction', {
+        code: lambda.Code.fromAsset("Resources/test_nodeJS.zip"),
+        runtime: lambda.Runtime.NODEJS_12_X,
         handler: 'index.handler',
-        code: new InlineCode('exports.handler = _ => "Hello, CDK";')
       });
     }
 }
